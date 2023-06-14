0d458f8a15fa204e4ff593930f46c4414d8ebdd9
Matías Macaya <matias.macaya@ug.uchile.cl>
2023-06-14 10:13:16 -0400

    now it can find multiple templates on image

--- find_cell_in_screenshot_test.py
+++ find_cell_in_screenshot_test.py
@@ -5,27 +5,52 @@ import time
 
 start_time = time.time()
 
-img_rgb = cv2.imread('.\images\minesweeper_map.png')
+# img_rgb = cv2.imread('.\images\minesweeper_map.png')
+# img_rgb = cv2.imread('./images/2023-06-12 (4).png')
+img_rgb = cv2.imread('./images/minesweeper_24x30_soclose.png')
+# img_rgb = cv2.imread('./images/2023-06-13 (14).png')
+# img_rgb = cv2.imread('./images/2023-06-11 (8).png')
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread('.\images\minesweeper_covered_cell.png',0)
-# template = cv2.imread('.\images\minesweeper_mine.png',0)
-# template = cv2.imread('.\images\minesweeper_flag.png',0)
-# template = cv2.imread('.\images\minesweeper_one.png',0)
-# template = cv2.imread('.\images\minesweeper_three.png',0)
-# template = cv2.imread('.\images\minesweeper_happy_face.png',0)
-w, h = template.shape[::-1]
 
-res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+search_files = [
+    '.\images\minesweeper_map_top_left_corner.png',
+    '.\images\minesweeper_map_bottom_right_corner.png',
+    # '.\images\minesweeper_covered_cell.png',
+    '.\images\minesweeper_empty_cell.png',
+    # '.\images\minesweeper_happy_face.png',
+    # '.\images\minesweeper_mine.png',
+    # '.\images\minesweeper_flag.png',
+    '.\images\minesweeper_one.png',
+    '.\images\minesweeper_two.png',
+    '.\images\minesweeper_three.png',
+    # '.\images\minesweeper_four.png',
+    # '.\images\minesweeper_five.png',
+    # '.\images\minesweeper_six.png',
+    # '.\images\minesweeper_seven.png',
+]
+
+prefix = '.\images\minesweeper_'
+suffix = '.png'
+
 threshold = 0.95
-loc = np.where( res >= threshold)
-# for pt in zip(*loc[::-1]):
-#     print(f"point: {pt}")
-#     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 
-# print()
+holder = []
+
+for n, file_name in enumerate(search_files):
+    template = cv2.imread(file_name,0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    loc = np.where( res >= threshold)
+    holder.append([n, w, h, loc, file_name])
+
+for n, w, h, loc, file_name in holder:
+    for pt in zip(*loc[::-1]):
+        print(f"point{n}:\t{pt}\t{file_name[len(prefix):-len(suffix)]}")
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+
 print(f"time: {time.time() - start_time}")
 
-cv2.imwrite('images/result.png',img_rgb)
+cv2.imwrite('images/_result.png',img_rgb)
 
 
 
