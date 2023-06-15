639b605b6e0c12408790c162675ea82b94aef18a
Matías Macaya <matias.macaya@ug.uchile.cl>
2023-06-14 22:47:40 -0400

    wip: refactor of 'minesweeper_ocr.py' in progress

--- minesweeper_ocr.py
+++ minesweeper_ocr.py
@@ -2,54 +2,37 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# img_rgb = cv2.imread('./images/2023-06-13 (14).png')
-img_rgb = cv2.imread('./images/minesweeper_24x30_soclose.png')
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template_top_left_corner = cv2.imread( '.\images\minesweeper_map_top_left_corner.png',0)
-template_bottom_right_corner = cv2.imread( '.\images\minesweeper_map_bottom_right_corner.png',0)
+# TODO: refactor - poner los tipos de las variables de entrada y salida de las funciones
 
 np.set_printoptions(linewidth=200)
-threshold = 0.95
-
-res = cv2.matchTemplate(img_gray,template_top_left_corner,cv2.TM_CCOEFF_NORMED)
-loc = np.where( res >= threshold)
-template_top_left_coord = np.concatenate(loc)[::-1]  # [value + 1 for value in list(zip(*loc[::-1]))[0]]
-
-res = cv2.matchTemplate(img_gray,template_bottom_right_corner,cv2.TM_CCOEFF_NORMED)
-loc = np.where( res >= threshold)
-template_bottom_right_coord = np.concatenate(loc)[::-1] # [value + 1 for value in list(zip(*loc[::-1]))[0]]
-
-top_left_corner = template_top_left_coord + np.array([9,17])
-bottom_right_corner = template_bottom_right_coord - np.array([1,1])
-cols, rows = (bottom_right_corner - top_left_corner) // 20
-player_map = np.empty([rows, cols], dtype=str)
-
-# TODO: agregar que si no encuentra alguno se detenga
-
-print("top_left_corner", top_left_corner)
-print("bottom_right_corner", bottom_right_corner)
-print("grid_size", cols, rows)
 
 # color in cv2
 # color     = np.array([ B , G , R ])
-white       = np.array([255,255,254])
-dark_gray   = np.array([131,131,131])
-red         = np.array([0,0,254])
-black       = np.array([0,0,0])
-gray        = np.array([192,192,192])
-colors = {
-    'gray': np.array([192,192,192]),
-    'red': np.array([0, 0, 254]),
-    'blue': np.array([254, 0, 0]),
-    'green': np.array([0, 128, 0]),
-    'purple': np.array([128, 0, 0]),
-    'light_purple': np.array([147, 58, 58]),
-    'burgundy': np.array([0, 0, 128]),
-    'cyan': np.array([128, 128, 0]),
-    'black': np.array([0,0,0])
+WHITE       = np.array([255,255,254])
+DARK_GRAY   = np.array([131,131,131])
+RED         = np.array([0,0,254])
+BLACK       = np.array([0,0,0])
+GRAY        = np.array([192,192,192])
+BLUE        = np.array([254, 0, 0])
+GREEN       = np.array([0, 128, 0])
+PURPLE      = np.array([128, 0, 0])
+LIGHT_PURPLE= np.array([147, 58, 58])
+BURGUNDY    = np.array([0, 0, 128])
+CYAN        = np.array([128, 128, 0])
+
+COLORS = {
+    'gray': GRAY,
+    'red': RED,
+    'blue': BLUE,
+    'green': GREEN,
+    'purple': PURPLE,
+    'light_purple': LIGHT_PURPLE,
+    'burgundy': BURGUNDY,
+    'cyan': CYAN,
+    'black': BLACK,
     
 }
-numbers = {
+NUMBERS = {
     'gray': "0",
     'blue': "1",
     'green': "2",
@@ -64,55 +47,107 @@ COVERED_CELL_CHAR = "."
 FLAG_CELL_CHAR = "●"
 MINE_CELL_CHAR = "*"
 
-white_tolerance = 10
-tolerance = 3
+def _get_grid_corners(img_rgb):
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template_top_left_corner = cv2.imread( '.\images\minesweeper_map_top_left_corner.png',0)
+    template_bottom_right_corner = cv2.imread( '.\images\minesweeper_map_bottom_right_corner.png',0)
 
-for row in range(rows):
-    for col in range(cols):
-        cell_pixel = top_left_corner + np.array([col, row]) * 20
-        # get image pixel
-        #   pixel_b, pixel_g, pixel_r = image[row][column]
-        x, y = cell_pixel + np.array([1,0])
-        pixel_0_1 = img_rgb[y][x]
-        # print(f"cell [{row},{col}] top left pixel ({cell_pixel}). Pixel ({x}, {y}) color  {pixel_0_1}.")
-        if np.allclose(pixel_0_1, white, atol=white_tolerance):
-            # covered cell: a flag or a non-pressed cell
-            # print(f"cell [{row},{col}] top left pixel ({cell_pixel}). Pixel ({x}, {y}) color  {pixel_0_1}. Covered.")
+    threshold = 0.95
+
+    res = cv2.matchTemplate(img_gray,template_top_left_corner,cv2.TM_CCOEFF_NORMED)
+    loc = np.where( res >= threshold)
+    template_top_left_coord = np.concatenate(loc)[::-1]
+
+    res = cv2.matchTemplate(img_gray,template_bottom_right_corner,cv2.TM_CCOEFF_NORMED)
+    loc = np.where( res >= threshold)
+    template_bottom_right_coord = np.concatenate(loc)[::-1]
+    
+    if template_bottom_right_coord.size == 0 or template_top_left_coord.size == 0:
+        return None, None
+
+    top_left_corner = template_top_left_coord + np.array([9,17])
+    bottom_right_corner = template_bottom_right_coord - np.array([1,1])
+    
+    return top_left_corner, bottom_right_corner
+
+def minesweeper_ocr(img_rgb):
+    top_left_corner, bottom_right_corner = _get_grid_corners(img_rgb)
+    if not top_left_corner.any() or not bottom_right_corner.any():
+        return None
+    
+    cols, rows = (bottom_right_corner - top_left_corner) // 20
+    player_map = np.empty([rows, cols], dtype=str)
+
+    print("top_left_corner", top_left_corner)
+    print("bottom_right_corner", bottom_right_corner)
+    print("grid_size", cols, rows)
+
+    white_tolerance = 10
+    tolerance = 3
+
+    for row in range(rows):
+        for col in range(cols):
+            cell_pixel = top_left_corner + np.array([col, row]) * 20
             
-            x9, y6 = cell_pixel + np.array([9,6])
-            _, y14 = cell_pixel + np.array([9,14])
-            pixel_9_6 = img_rgb[y6][x9]
-            pixel_9_14 = img_rgb[y14][x9]
-            if np.allclose(pixel_9_6, red, atol=3) and np.allclose(pixel_9_14, black, atol=tolerance):
-                # is flag
-                player_map[row, col] = FLAG_CELL_CHAR
-            if np.allclose(pixel_9_6, gray, atol=3) and np.allclose(pixel_9_14, gray, atol=tolerance):
-                # is covered cell
-                player_map[row, col] = COVERED_CELL_CHAR
-                print(f"cell [{row},{col}] Covered. Pixel colors ({x9},{y6}) {pixel_9_6} ({x9},{y14}) {pixel_9_14}.")
-        #     else:
-        #         print("ERROR: no cell match for colors")
-        elif np.allclose(pixel_0_1, dark_gray, atol=tolerance):
-            # uncovered cell: a number, a mine or an empty cell
-            # print(f"cell [{row},{col}] top left pixel ({cell_pixel}). Pixel ({x}, {y}) color  {pixel_0_1}. Uncovered.")
-            x, y = cell_pixel + np.array([11,14])
-            pixel_11_14 = img_rgb[y][x]            
-            # np.linalg.norm()
-            matched_colors = []
-
-            for color_name, color_value in colors.items():
-                if np.allclose(pixel_11_14, color_value, atol=tolerance):
-                    matched_colors.append(color_name)
-                        
-            if len(matched_colors) != 1:
-                print(f"ERROR: more than one color detected. Cell [{row},{col}] Pixel ({x},{y}) colors {matched_colors}")
-                break
+            # get image pixel -> pixel_b, pixel_g, pixel_r = image[row][column]
+            x, y = cell_pixel + np.array([1,0])
+            pixel_0_1 = img_rgb[y][x]
+            # print(f"cell [{row},{col}] top left pixel ({cell_pixel}). Pixel ({x}, {y}) color  {pixel_0_1}.")
             
-            number = numbers[matched_colors[0]]
-            player_map[row, col] = number
-            # print(f"cell [{row},{col}] Uncovered. Pixel ({x}, {y}) #{number} color  {matched_colors} {pixel_11_14}.")
+            if np.allclose(pixel_0_1, WHITE, atol=white_tolerance):
+                # print(f"cell [{row},{col}] top left pixel ({cell_pixel}). Pixel ({x}, {y}) color  {pixel_0_1}. Covered.")
+                x9, y6 = cell_pixel + np.array([9,6])
+                _, y14 = cell_pixel + np.array([9,14])
+                pixel_9_6 = img_rgb[y6][x9]
+                pixel_9_14 = img_rgb[y14][x9]
+                if np.allclose(pixel_9_6, RED, atol=3) and np.allclose(pixel_9_14, BLACK, atol=tolerance):
+                    player_map[row, col] = FLAG_CELL_CHAR
+                elif np.allclose(pixel_9_6, GRAY, atol=3) and np.allclose(pixel_9_14, GRAY, atol=tolerance):
+                    player_map[row, col] = COVERED_CELL_CHAR
+                    print(f"cell [{row},{col}] Covered. Pixel colors ({x9},{y6}) {pixel_9_6} ({x9},{y14}) {pixel_9_14}.")
+                else:
+                    print("ERROR: no cell match for colors")
+            elif np.allclose(pixel_0_1, DARK_GRAY, atol=tolerance):
+                # print(f"cell [{row},{col}] top left pixel ({cell_pixel}). Pixel ({x}, {y}) color  {pixel_0_1}. Uncovered.")
+                x, y = cell_pixel + np.array([11,14])
+                pixel_11_14 = img_rgb[y][x]            
+                
+                matched_colors = []
+                for color_name, color_value in COLORS.items():
+                    if np.allclose(pixel_11_14, color_value, atol=tolerance):
+                        matched_colors.append(color_name)
+                            
+                if len(matched_colors) != 1:
+                    print(f"ERROR: more than one color detected. Cell [{row},{col}] Pixel ({x},{y}) colors {matched_colors}")
+                    return None
+                
+                number = NUMBERS[matched_colors[0]]
+                player_map[row, col] = number
+                # print(f"cell [{row},{col}] Uncovered. Pixel ({x}, {y}) #{number} color  {matched_colors} {pixel_11_14}.")
+    
+    return player_map
 
-# print(player_map)
-print(np.array2string(player_map, separator=' ', formatter={'str_kind': lambda x: x}))
+def get_screenshot(window_title: str = ""):
+    # TODO: usar funcion del modulo screenshot
+    img_rgb = None
+    # if not window_title:
+    img_rgb = cv2.imread('./images/minesweeper_24x30_soclose.png')
+    # img_rgb = cv2.imread('./images/2023-06-12 (4).png')
+    return img_rgb
 
+def display_array(arr):
+    print(np.array2string(arr, separator=' ', formatter={'str_kind': lambda x: x}))
 
+def main():
+    game_image = get_screenshot("Minesweeper X")
+    if not game_image.any():
+        return
+    
+    player_map = minesweeper_ocr(game_image)
+    if player_map is None:
+        return
+    
+    display_array(player_map)
+        
+if __name__ == '__main__':
+    main()
