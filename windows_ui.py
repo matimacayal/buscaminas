c105421df7d10e340bd730857f63a36d9c9342b9
Matías Macaya <matias.macaya@ug.uchile.cl>
2023-06-14 22:46:58 -0400

    new feature now clicks in Minesweeper X cells

--- windows_ui.py
+++ windows_ui.py
@@ -33,8 +33,44 @@ def take_screenshot(window_title):
     
     # return grayscale_array
 
+def click_window(window_title, x, y):
+    window_info = pyautogui.getWindowsWithTitle(window_title)
+    if len(window_info) == 0:
+        print("Window not found.")
+        return
+    window = window_info[0]
+    window_left = window.left
+    window_top = window.top
+
+    absolute_x = window_left + x
+    absolute_y = window_top + y
+
+    pyautogui.click(absolute_x, absolute_y)
+
+def click_cell(row, col):
+    grid_top = 127
+    grid_left = 16
+    x = col * 20 + 10 + grid_left
+    y = row * 20 + 10 + grid_top
+    click_window('Minesweeper X', x, y)
+
+def play_minesweeper():
+    while 1:
+        txt = input("input:")
+        if "e" in txt.lower():
+            break
+        elif "r" in txt.lower():
+            click_window('Minesweeper X', 315, 91)
+            click_window('Minesweeper X', 315, 91)
+            continue
+        r, c = txt.split(",")
+        r, c = [int(r), int(c)]
+        click_cell(r,c)
+
 def main():
-    take_screenshot('Minesweeper X')
+    # click_window('Minesweeper X', 226, 175)
+    # take_screenshot('Minesweeper X')
+    play_minesweeper()
 
 if __name__ == '__main__':
     main()
